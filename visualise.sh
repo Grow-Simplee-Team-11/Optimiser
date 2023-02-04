ee3d3dfb8a64d4676c9e1efdbbf828745c723abe
Ubuntu <ubuntu@ip-172-31-16-166.ap-south-1.compute.internal>
2023-02-04 12:32:53 +0000

    nbiter and epsilon changed

--- visualise.sh
+++ visualise.sh
@@ -1,2 +1,1 @@
-sudo docker cp $(sudo docker ps -q | head -n 1):/Optimizer/cuboids_to_render.xml ./visual/app/cuboids_to_render.xml
-cd ./visual/app/ && python3 -m http.server 8005 
+sudo docker cp $(sudo docker ps -q | head -n 1):/Optimizer/tests/cluster.txt ./cluster.txt
