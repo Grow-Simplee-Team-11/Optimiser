213f8030d846e30125e0342e200059a3c5df4f70
Ubuntu <ubuntu@ip-172-31-16-166.ap-south-1.compute.internal>
2023-02-07 00:05:16 +0000

    update code

--- src/clustering/HGS/Genetic.cpp
+++ src/clustering/HGS/Genetic.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 void GeneticParallel::run() {
     /* INITIAL POPULATION */
-    population.generatePopulation();
+    population->generatePopulation();
 
     int nbIter;
     int nbIterNonProd = 1;
@@ -21,15 +21,15 @@ void GeneticParallel::run() {
         //------------Select Parents ----------------------//////
         vector<const Individual*> parents;
         for (int i = 0; i < this->nThreads; i++) {
-            parents.push_back(&population.getBinaryTournament());
-            parents.push_back(&population.getBinaryTournament());
+            parents.push_back(&population->getBinaryTournament());
+            parents.push_back(&population->getBinaryTournament());
         }
 
         //------------Crossover and Split ----------------------//
         vector<thread> threads;
         for (int i = 0; i < this->nThreads; i++) {
             threads.push_back(thread(&GeneticParallel::crossoverOX, this,
-                                     ref(offsprings[i]), ref(*parents[2 * i]),
+                                     ref(*offsprings[i]), ref(*parents[2 * i]),
                                      ref(*parents[2 * i + 1]),i));
         }
         for (int i = 0; i < this->nThreads; i++) {
@@ -43,7 +43,7 @@ void GeneticParallel::run() {
             auto fn = [](LocalSearch& ls, Individual& ind, double penaltyCapacity, double penaltyDuration) {
                 ls.run(ind, penaltyCapacity, penaltyDuration);
             };
-            threads.push_back(thread(fn, ref(localSearches[i]), ref(offsprings[i]),
+            threads.push_back(thread(fn, ref(*localSearches[i]), ref(*offsprings[i]),
                                      params.penaltyCapacity, params.penaltyDuration));
         }
         for (int i = 0; i < this->nThreads; i++) {
@@ -54,19 +54,19 @@ void GeneticParallel::run() {
         // --------------Add to Population ----------------------//
         vector<bool> isNewBests;
         for (int i = 0; i < this->nThreads; i++) {
-            isNewBests.push_back(population.addIndividual(offsprings[i], true));
+            isNewBests.push_back(population->addIndividual(*offsprings[i], true));
         }
 
         // --------------Repair Infeasible ----------------------//
         vector<int> infeasible;
         for (int i = 0; i < this->nThreads; i++) {
-            if (!offsprings[i].eval.isFeasible && params.ran() % 2 == 0) infeasible.push_back(i);
+            if (!offsprings[i]->eval.isFeasible && params.ran() % 2 == 0) infeasible.push_back(i);
         }
         for (int i = 0; i < infeasible.size(); i++) {
             auto fn = [](LocalSearch& ls, Individual& ind, double penaltyCapacity, double penaltyDuration) {
                 ls.run(ind, penaltyCapacity, penaltyDuration);
             };
-            threads.push_back(thread(fn,ref(localSearches[i]), ref(offsprings[i]),params.penaltyCapacity * 10., params.penaltyDuration * 10.));
+            threads.push_back(thread(fn,ref(*localSearches[i]), ref(*offsprings[i]),params.penaltyCapacity * 10., params.penaltyDuration * 10.));
         }
         for (int i = 0; i < infeasible.size(); i++) {
             threads[i].join();
@@ -75,7 +75,7 @@ void GeneticParallel::run() {
         
         // --------------Add to Population ----------------------//
         for (int i = 0; i < infeasible.size(); i++) {
-            isNewBests[infeasible[i]] = (population.addIndividual(offsprings[infeasible[i]], false) ||
+            isNewBests[infeasible[i]] = (population->addIndividual(*offsprings[infeasible[i]], false) ||
                                          isNewBests[infeasible[i]]);
         }
         bool isNewBest = false;
@@ -97,12 +97,12 @@ void GeneticParallel::run() {
             nbIterNonProd++;
 
         /* DIVERSIFICATION, PENALTY MANAGEMENT AND TRACES */
-        if (nbIter % params.ap.nbIterPenaltyManagement == 0) population.managePenalties();
-        if (nbIter % params.ap.nbIterTraces == 0) population.printState(nbIter, nbIterNonProd);
+        if (nbIter % params.ap.nbIterPenaltyManagement == 0) population->managePenalties();
+        if (nbIter % params.ap.nbIterTraces == 0) population->printState(nbIter, nbIterNonProd);
 
         /* FOR TESTS INVOLVING SUCCESSIVE RUNS UNTIL A TIME LIMIT: WE RESET THE ALGORITHM/POPULATION EACH TIME maxIterNonProd IS ATTAINED*/
         if (params.ap.timeLimit != 0 && nbIterNonProd == params.ap.nbIter) {
-            population.restart();
+            population->restart();
             nbIterNonProd = 1;
         }
     }
@@ -197,7 +197,7 @@ void GeneticParallel::crossoverOX(Individual& result, const Individual& parent1,
     // Frequency table to track the customers which have been already inserted
     if (params.nbClients == 1) {
         result = parent1;
-        splits[idx].generalSplit(result, parent1.eval.nbRoutes);
+        splits[idx]->generalSplit(result, parent1.eval.nbRoutes);
         return;
     }
 
@@ -228,14 +228,20 @@ void GeneticParallel::crossoverOX(Individual& result, const Individual& parent1,
     }
 
     // Complete the individual with the Split algorithm
-    splits[idx].generalSplit(result, parent1.eval.nbRoutes);
+    splits[idx]->generalSplit(result, parent1.eval.nbRoutes);
 }
 
-GeneticParallel::GeneticParallel(Params& params, int _nThreads) : nThreads(_nThreads), params(params), 
-                                                                 splits(std::vector<Split>(nThreads, Split(params))),
-                                                                 localSearches(std::vector<LocalSearch>(nThreads, LocalSearch(params))),
-                                                                 population(params,splits[0],localSearches[0]) {
-    this->offsprings = std::vector<Individual>(nThreads, Individual(params));
+GeneticParallel::GeneticParallel(Params& params, int _nThreads) : nThreads(_nThreads), params(params){
+    this->splits = vector<Split*>(nThreads);
+    this->localSearches = vector<LocalSearch*>(nThreads);
+    this->offsprings = vector<Individual*>(nThreads);
+    for (int i = 0; i < nThreads; i++){
+        this->splits[i] = new Split(params);
+        this->localSearches[i] = new LocalSearch(params);
+        this->offsprings[i] = new Individual(params);
+    }
+    this->population = new Population(params,*splits[0],*localSearches[0]);
+    
 }
 
 /**
